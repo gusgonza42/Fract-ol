876b37ebec753ede2f1ebd5a58514d365617063f
gusgonza <gusgonza@student.42barcelona.com>
2024-09-28 13:10:45 +0200

    main refactor

--- ft_main.c
+++ ft_main.c
@@ -11,30 +11,62 @@
 /* ************************************************************************** */
 
 #include "fractol.h"
-#include "minilibx-linux/mlx.h"
 
+void	my_mlx_pixel_put(t_img *data, int x, int y, int color);
 
-int main(int ac, char *av[])
+int	main(int ac, char *av[])
 {
-	t_fractal fractal;
+	t_fractal	fractal;
 
-	if (2 == ac && !ft_strncmp(av[1], "mandelbrot", 10)
-		|| 4 == ac && !ft_strncmp(av[1], "julia", 5))
+	if (2 == ac && !ft_strncmp(av[1], "mandelbrot", 11))
 	{
-		printf("init()\n");
 		fractal.name = av[1];
 		fractal_init(&fractal);
 		fractal_render(&fractal);
 		mlx_loop(fractal.mlx_connection);
-
+	}
+	else if (4 == ac && !ft_strncmp(av[1], "julia", 6) && ft_chkr([2])
+		&& ft_chkr(av[3]))
+	{
+		fractal.name = av[1];
+		fractal.julia_x = ft_atodbl(av[2]);
+		fractal.julia_y = ft_atodbl(av[3]);
+		fractal_init(&fractal);
+		fractal_render(&fractal);
+		mlx_loop(fractal.mlx_connection);
 	}
 	else
 	{
-		putstr_fd(ERROR_MESSAGE, STDERR_FILENO); //TODO
-		exit (EXIT_FAILURE);
-		malloc_error();
-
+		ft_putstr_fd(ERRORMSSG1 + ERRORMSSG2 + ERRORMSSG3, 2);
+		exit(EXIT_FAILURE);
 	}
 	return (0);
-
 }
+
+void	my_mlx_pixel_put(t_img *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+/*
+int	main(void)
+{
+	t_complex	c;
+	t_complex	z;
+	t_complex	tmp_real;
+
+	z.real = 0;
+	z.imaginary = 0;
+	c.real = 0.285;
+	c.imaginary = 0.01;
+	for (int i = 0; i < 100; i++)
+	{
+		tmp_real.real = z.real;
+		z.real = z.real * z.real - z.imaginary * z.imaginary + c.real;
+		z.imaginary = 2 * tmp_real.real * z.imaginary + c.imaginary;
+		printf("z.real: %f\nz.imaginary: %f\n", z.real, z.imaginary);
+	}
+}
+*/
