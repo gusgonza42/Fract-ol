ef49aff90d40752f0724b6b94283571c027bfe07
gusgonza <gusgonza@student.42barcelona.com>
2024-09-28 12:48:02 +0200

    update header

--- fractol.h
+++ fractol.h
@@ -9,138 +9,96 @@
 /*   Updated: 2024/09/25 12:09:47 by gusgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-# include <stdio.h> //TODO debugging
-# include <stdlib.h> //malloc free
-# include <unistd.h> // write
-# include <math.h>
-# include <X11/X.h>
-# include <X11/keysym.h>
+# include "libft/libft.h"
 # include "minilibx-linux/mlx.h"
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
+# define ERRORMSSG1 "Please, enter a valid fractal name: \n"
+# define ERRORMSSG2 "\t\"mandelbrot\"\n"
+# define ERRORMSSG3 "\t\"julia <val_1> <val_2>\"\n"
 
-#define ERROR_MESSAGE "Please enter \n\t\"./fractol mandelbrot\" or \n\t\"./fractol julia <value_1> <value_2>\"\n"
+# define WIDTH 800
+# define HEIGHT 800
 
-/*
- * We use a square to 
- * keep the rendering math simple
-*/
-#define WIDTH	800
-#define	HEIGHT	800
+/*<-- COLORS -->*/
+# define BLACK 0x000000
+# define WHITE 0xFFFFFF
+# define RED 0xFF0000
+# define GREEN 0x00FF00
+# define BLUE 0x0000FF
 
-/*
- * COLORS
-*/
-#define BLACK       0x000000  // RGB(0, 0, 0)
-#define WHITE       0xFFFFFF  // RGB(255, 255, 255)
-#define RED         0xFF0000  // RGB(255, 0, 0)
-#define GREEN       0x00FF00  // RGB(0, 255, 0)
-#define BLUE        0x0000FF  // RGB(0, 0, 255)
+/*<-- SPECIAL COLORS -->*/
+# define MAGENTABURTS 0xFF00FF // RGB(255, 0, 255)
+# define LIMESHOCK 0x32CD32 // RGB(50, 205, 50)
+# define NEON_ORANGE 0xFF4500 // RGB(255, 69, 0)
+# define ACID_GREEN 0xB0FF00 // RGB(176, 255, 0)
+# define ELECTRIC_BLUE 0x7DF9FF // RGB(125, 249, 255)
+# define ULTRA_VIOLET 0x6A0DAD // RGB(106, 13, 173)
+# define PSY_YELLOW 0xFFFF33 // RGB(255, 255, 51)
+# define FLUORESCENT_PINK 0xFF1493 // RGB(255, 20, 147)
+# define LASER_LEMON 0xFFFF66 // RGB(255, 255, 102)
+# define HOT_PINK 0xFF69B4 // RGB(255, 105, 180)
+# define RADIANT_RED 0xFF5555 // RGB(255, 85, 85)
 
-// Psychedelic colors
-#define MAGENTA_BURST   0xFF00FF  // A vibrant magenta
-#define LIME_SHOCK      0xCCFF00  // A blinding lime
-#define NEON_ORANGE     0xFF6600  // A blazing neon orange
-#define PSYCHEDELIC_PURPLE 0x660066  // A deep purple
-#define AQUA_DREAM      0x33CCCC  // A bright turquoise
-#define HOT_PINK        0xFF66B2  // As the name suggests!
-#define ELECTRIC_BLUE   0x0066FF  // A radiant blue
-#define LAVA_RED        0xFF3300  // A bright, molten red
+/* KEY CODES*/
+# define K_ESC 0xff1b
+# define K_LEFT 0xff51
+# define K_UP 0xff52
+# define K_RIGHT 0xff53
+# define K_DOWN 0xff54
+# define K_PLUS 65451
+# define K_MINUS 65453
 
-
-
-
-
-/*
- * COMPLEX value
-*/
-typedef struct	s_complex
+/*<-- STRUCTS -->*/
+typedef struct s_complex
 {
-	//		real
-	double	x;
-	//		i
-	double	y;
-}				t_complex;
+	double	re;
+	double	im;
+}			t_complex;
 
-
-
-/*
- * IMAGE
- * This is basically a pixels buffer
- * values from mlx_get_data_addr()
-*/
-typedef struct	s_img
+typedef struct s_img
 {
-	void	*img_ptr; //pointer to image struct
-	char	*pixels_ptr; //points to the actual pixels
+	void	*img;
+	char	*addr;
 	int		bpp;
-	int		endian;
 	int		line_len;
-}				t_img;
+	int		endian;
+}			t_img;
 
-
-/*
- * FRACTAL ID
- * ~ MLX stuff
- * ~ Image 
- * ~ Hooks values
-*/
-typedef struct	s_fractal
+typedef struct s_fractal
 {
 	char	*name;
-	//MLX
-	void	*mlx_connection; // mlx_init()
-	void	*mlx_window; 	 // mlx_new_window()
-	//Image
+	void	*mlx_connection;
+	void	*mlx_window;
 	t_img	img;
-
-	//Hooks member variables //TODO
-	double	escape_value; // hypotenuse
-	int		iterations_defintion; // value tight with the image quality and rendering speed
-	double	shift_x;
-	double	shift_y;
+	double	x_shift;
+	double	y_shift;
 	double	zoom;
 	double	julia_x;
 	double	julia_y;
-}				t_fractal;
+}			t_fractal;
 
-
-
-
-
-
-
-
-
-
-/*
- * PROTOTYPES
- * They are basically IOUs to the compiler
-*/
-//*** strings utils ***
-int		ft_strncmp(char *s1, char *s2, int n);
-void    putstr_fd(char *s, int fd);
-double  atodbl(char *s);
-
-//*** init ***
-void    fractal_init(t_fractal *fractal);
-
-//*** render ***
-void    fractal_render(t_fractal *fractal);
-
-//*** math ***
-double		map(double unscaled_num, double new_min, double new_max, double old_min, double old_max);
-t_complex   sum_complex(t_complex z1, t_complex z2);
-t_complex   square_complex(t_complex z);
-
-//*** hooks_events ***
-int			key_handler(int keysym, t_fractal *fractal);
-
-//*** clean_up ***
-int    	close_handler(t_fractal *fractal);
-int		mouse_handler(int button, int x, int y, t_fractal *fractal);
-int		julia_track(int x, int y, t_fractal *fractal);
+// Init
+void		fractal_init(t_fractal *fractal);
+// Math
+double		mapscale(double unsc_num, double n_min, double n_max, double o_max);
+t_complex	sum_complex(t_complex z1, t_complex z2);
+t_complex	sqr_complex(t_complex z);
+double		ft_atoidbl(char *str);
+int			ft_chkr(char *str);
+// Render
+void		fractal_render(t_fractal *fractal);
+// Events
+int			key_handler(int keycode, t_fractal *fractal);
+int			mouse_handler(int button, int x, int y, t_fractal *fractal);
+int			close_handler(t_fractal *fractal);
+int			motion_handler(int x, int y, t_fractal *frt);
 
 #endif
